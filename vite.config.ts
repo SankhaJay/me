1b030c893498179f2c95c33bb781f28ab2064c8c
Sankha Jayalath <sankha.rc@gmail.com>
2025-03-20 22:06:34 +0530

    edit vite configs

--- vite.config.ts
+++ vite.config.ts
@@ -14,10 +14,10 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  // resolve: {
+  //   alias: {
+  //     "@": path.resolve(__dirname, "./src"),
+  //   },
+  // },
   base: "/me",
 }));
